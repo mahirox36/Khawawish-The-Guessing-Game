f4e776a500a0ca68a76dcda592a939520ddb42f3
Mahiro <mahiroc36@gmail.com>
2025-12-30 00:33:37 +0300

    restored it

--- main.py
+++ main.py
@@ -220,14 +220,12 @@ def get_static_file_names(count: int = 578):
 static_file_names = get_static_file_names()
 
 
-def _pre_hash(password: str) -> str:
-    return hashlib.sha256(password.encode("utf-8")).hexdigest()
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(_pre_hash(password))
+    return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(_pre_hash(plain_password), hashed_password)
+    return pwd_context.verify(plain_password, hashed_password)
 
 
 def create_access_token(data: dict) -> str:
